d89a82d1fd18a4c2670d1cbfda12945c093564f3
xiao <535408893@qq.com>
2021-08-11 10:18:10 +0800

    complete mock task

--- test/mock/SecurityCenter.test.js
+++ test/mock/SecurityCenter.test.js
@@ -1,12 +1,15 @@
 import DoorPanel from "../../src/mock/DoorPanel";
 import SecurityCenter from "../../src/mock/SecurityCenter";
+jest.mock("../../src/mock/DoorPanel");
 
 describe('security center test', () => {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-
+    const doorPanel = new DoorPanel();
+    doorPanel.close.mockImplementation(() => "Door is closed");
     test('should verify door is closed', () => {
-        expect(true).toBe(false);
+        new SecurityCenter(doorPanel).switchOn()
+        expect(doorPanel.close).toBeCalled();
     });
 });
